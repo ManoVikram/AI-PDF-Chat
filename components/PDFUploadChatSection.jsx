c7fd96726676f1451dfda6141dfe53e89cb9ba68
Mano Vikram <manovik18@gmail.com>
2025-09-19 01:55:48 +0530

    Added the code to add functionality to drag and drop a PDF file or choose a PDF file with the picker

--- components/PDFUploadChatSection.jsx
+++ components/PDFUploadChatSection.jsx
@@ -1,15 +1,62 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const PDFUploadChatSection = () => {
+    const [fileName, setFileName] = useState(null)
+    const [isDragging, setIsDragging] = useState(null)
+
+    const inputFileRef = useRef(null)
+
+    const handleDragOver = (e) => {
+        e.preventDefault()
+        setIsDragging(true)
+    }
+
+    const handleDragLeave = (e) => {
+        e.preventDefault()
+        setIsDragging(false)
+    }
+
+    const handleDrop = (e) => {
+        e.preventDefault()
+        setIsDragging(false)
+
+        const file = e.dataTransfer.files[0]
+
+        if (file && file.type === 'application/pdf') {
+            setFileName(file.name)
+        } else {
+            alert('Please drop a valid PDF file.')
+        }
+    }
+
+    const handleFilePick = (e) => {
+        const file = e.target.files[0]
+
+        if (file && file.type === 'application/pdf') {
+            setFileName(file.name)
+        } else {
+            alert('Please select a valid PDF file.')
+        }
+    }
+
+    const openFileDialog = () => inputFileRef.current.click()
+
     return (
         <section className="flex flex-row size-full space-x-6 pt-4">
-            <div className="flex flex-col flex-1 justify-center items-center w-full rounded-4xl bg-gray-50 space-y-3">
-                <Image src="PDFFileIcon.svg" height={40} width={40} />
+            <div className={`flex flex-col flex-1 justify-center items-center w-full rounded-4xl space-y-3 transition-colors ${isDragging ? "bg-gray-50" : "bg-left-pane"}`} onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop}>
+                <Image src="PDFFileIcon.svg" alt='pdf-icon' height={40} width={40} />
 
-                <p>Drop and drop your files here or <span className='font-bold underline cursor-pointer'>Choose file</span></p>
+                <p>Drop and drop your files here or <span className='font-bold underline cursor-pointer' onClick={openFileDialog}>Choose file</span></p>
+
+                <input type="file" ref={inputFileRef} className='hidden' accept='application/pdf' onChange={handleFilePick} />
             </div>
-            <div className="flex-1 bg-yellow-200"></div>
+
+            <div className="flex flex-col flex-1 justify-center items-center w-full rounded-4xl bg-right-pane space-y-3">
+
+            </div>
         </section>
     )
 }
